e401019bbc5086dc3c6b3409916344ede1aed5a7
西米 <111104984+1099234990@users.noreply.github.com>
2022-09-14 17:22:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -92,13 +92,6 @@ def get_daily_eng():
   res = conn.getresponse()
   data = res.read()
   print(data.decode('utf-8'))
-  result=data.decode('utf-8')
-  fin=json.loads(result)
-  doc=''
-  if fin['code']==200:
-    doc=fin['newslist'][0]['content'] + fin['newslist'][0]['note']
-    print(doc)
-  return doc
 
 
 
